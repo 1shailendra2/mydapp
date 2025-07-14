0013021356fcc732b666ad2d872f50ac6fa096ed
1shailendra2 <bhandarishailesh81@gmail.com>
2025-07-14 15:58:26 +0545

    changed abi from manual to hardhat automatic

--- tokentransfer/backend/hardhat.config.js
+++ tokentransfer/backend/hardhat.config.js
@@ -1,6 +1,8 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 require("@nomicfoundation/hardhat-toolbox");
+
 require("dotenv").config();
+const { PRIVATE_KEY, RPC_URL } = process.env;
 module.exports = {
   solidity: "0.8.28",
   networks: {
--- tokentransfer/backend/imp/contra.js
+++ tokentransfer/backend/imp/contra.js
@@ -1,16 +1,13 @@
 const {ethers}= require("ethers");
-require("dotenv").config();
+require("dotenv").config({ path: __dirname + "/../.env" });
 console.log("Private Key Raw:", process.env.PRIVATE_KEY);
 console.log("Length:", process.env.PRIVATE_KEY.length);
 const provider= new ethers.JsonRpcProvider(process.env.RPC_URL);
 const PRIVATE_KEY=process.env.PRIVATE_KEY;
 const wallet= new ethers.Wallet(PRIVATE_KEY,provider);
 const CONTRACT_ADDRESS="0xb2A63962408E48E5E95feeD0821337804c261167";
-const ABI=[
-    "function transfer(address to, uint amount) public returns (bool)",
-    "function balanceOf(address) view returns (uint)",
-    "function faucet() public",
-];
+const artifact=require("../artifacts/contracts/token.sol/MyToken.json");
+const ABI=artifact.abi;
 const contract=new ethers.Contract(CONTRACT_ADDRESS,ABI,wallet);
 async function sendTokens(to, amount){
     const tx=await contract.transfer(to,ethers.parseUnits(amount,18));
